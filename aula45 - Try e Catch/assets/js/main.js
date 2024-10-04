12155e58607d8cb0d84ac2fcb502eeaa0a5d0a0a
Telles0203 <alexandre@tellesadvocacia.com.br>
2024-10-04 09:17:47 -0300

    Aulas de java intermediário

---
+++ aula45 - Try e Catch/assets/js/main.js
@@ -0,0 +1,7 @@
+try{
+  console.log(naoExisto);
+
+}catch(err){
+  console.log('nãoExisto Não existe');
+  console.log(err);
+}
---
+++ aula46 - tratando e lançando erros (try, catch, finally)/assets/js/main.js
@@ -0,0 +1,8 @@
+try{
+//É executando quando não tem erros.
+
+}catch(err){
+//É executado quando há erros
+} finally{
+  //Sempre
+}
